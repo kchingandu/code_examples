6185508104885b548d7ed57f31130eeaa7b00fbe
kchingandu <kchingandu@users.noreply.github.com>
2017-01-03 11:54:03 +0000

    Update ModalReducer.js

--- react_components/Modal/ModalReducer.js
+++ react_components/Modal/ModalReducer.js
@@ -2,7 +2,6 @@ const modalConfig = (state = { isOpen: false, content: '', contentData: {} }, { 
     switch (type) {
         case 'UPDATE_MODAL_CONFIG':
             return Object.assign({}, state, modalConfig);
-
         default:
             return state
     }
